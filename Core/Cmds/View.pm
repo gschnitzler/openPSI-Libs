656cd9b1edb599ce2b7ff105f419a1f4c8c34660
Gerulf Schnitzler <github.account@goto11.at>
2024-04-14 14:00:15 +0200

    formatting

--- Core/Cmds/View.pm
+++ Core/Cmds/View.pm
@@ -6,14 +6,12 @@ use Data::Dumper;
 
 use Tree::Search qw(tree_search_position tree_search_deep);
 
-# Export
 our @EXPORT_OK = qw(import_view);
 
-#
 # this module provides insights into the DATA part of a particular command.
-# usefull to trace bugs relating wrong assumptions about data.
+# useful to trace bugs relating wrong assumptions about data.
 # this module is old and full of little quirks, but performs well enough
-# state variables where introduced long after this, so it lacks the capability to resolve CODE refrences
+# state variables were introduced long after this, so it lacks the capability to resolve CODE references
 ##########################################################################################
 
 sub _query ( $data, @keys ) {
@@ -30,19 +28,16 @@ sub _query ( $data, @keys ) {
 
     # add 'cmds' to key list to resemble the $core structure, thus other data (like id or config) is not accessible
     return tree_search_position( $data, $cond, \@keys );
-
 }
 
 sub _query_wrapper ( $query, $args ) {
 
-    # we get all the data, so we avoid die if data is not found;
-    my $result = $query->();
+    my $result = $query->();    # we get all the data, so we avoid die if data is not found;
 
     return $result unless ( $args->[0] );
 
     my ( $hit, $misses ) = tree_search_deep( $result, sub { return 1; }, $args );
 
-    #  say Dumper $hit, $misses;
     say 'queried too deep or data not found, structure ended after: ', join( ' ', $misses->@* ) if $misses;
     return $hit;
 }
@@ -58,11 +53,11 @@ sub view ( $data, @keys ) {
 
     my $result = _query_wrapper( $query->{DATA}, $args );
 
-    $Data::Dumper::Indent = 1;
-    $Data::Dumper::Terse  = 1;
-
-    say Dumper $result if ($result);
-
+    {
+        local $Data::Dumper::Indent = 1;
+        local $Data::Dumper::Terse  = 1;
+        say Dumper $result if ($result);
+    }
     return;
 }
 
@@ -75,8 +70,7 @@ sub list_keys ( $data, @keys ) {
         return;
     }
 
-    # normal query interface
-    my $result = _query_wrapper( $query->{DATA}, $args );
+    my $result = _query_wrapper( $query->{DATA}, $args );    # normal query interface
 
     unless ($result) {
         say 'data not found';
@@ -86,10 +80,9 @@ sub list_keys ( $data, @keys ) {
     if ( ref $result ne 'HASH' ) {
         say 'ERROR: queried too deep, not a hash reference.';
         return;
-
     }
-    say foreach ( keys $result->%* );
 
+    say foreach ( keys $result->%* );
     return;
 }
 
@@ -106,16 +99,11 @@ sub import_view ($core) {
                 view( $core, @args );
             },
             DESC => 'Prints a commands data tree.',
-            HELP => [
-                'usage:', 'use \'help\' to get a command overview, then type',
-                '', 'view <full cmd> [args]',
-                '',
-                'to query a commands dataset.',
-            ],
+            HELP => [ 'usage:', 'use \'help\' to get a command overview, then type', '', 'view <full cmd> [args]', '', 'to query a commands dataset.', ],
             DATA => {}
         },
         keys => {
-            CMD => sub(@args) {
+            CMD => sub (@args) {
                 shift @args;    # contains $query
                 list_keys( $core, @args );
             },
@@ -127,5 +115,3 @@ sub import_view ($core) {
 
     return ($struct);
 }
-1;
-
--- Core/Plugins/Macros.pm
+++ Core/Plugins/Macros.pm
@@ -6,13 +6,13 @@ use Data::Dumper;
 use Storable qw (nstore);
 
 use Core::Plugins::Condition qw(plugin_condition);
-use Core::Shell::Variables qw(replace_variables);
-use Core::Shell::Input qw(filter_input);
+use Core::Shell::Variables   qw(replace_variables);
+use Core::Shell::Input       qw(filter_input);
 
 use Core::Query qw(query);
 
-use Tree::Slice qw(slice_tree);
-use Tree::Merge qw(add_tree);
+use Tree::Slice  qw(slice_tree);
+use Tree::Merge  qw(add_tree);
 use PSI::Console qw(print_table);
 
 our @EXPORT_OK = qw(load_macros);
@@ -24,7 +24,7 @@ my $macro_ref = {
                         # never write clever code at 2am
 };
 
-sub _check_macro_buildins($queue) {
+sub _check_macro_buildins ($queue) {
 
     my $buildin = {
         SAVE       => 0,
@@ -35,6 +35,7 @@ sub _check_macro_buildins($queue) {
     # there could be multiple SAVE and CONTINUE statements inside a queue (for whatever reason)
     # but there should be the same amount of both
     while ( my $item = shift $queue->@* ) {
+
         if ( $item eq 'CONTINUE' ) {
             $buildin->{CONTINUE}++;
         }
@@ -45,12 +46,12 @@ sub _check_macro_buildins($queue) {
         }
     }
     die 'ERROR: no equal amount of SAVE and CONTINUE statements.'
-        unless ( ( $buildin->{SAVE} + $buildin->{SAVECHROOT} ) == $buildin->{CONTINUE} );
+      unless ( ( $buildin->{SAVE} + $buildin->{SAVECHROOT} ) == $buildin->{CONTINUE} );
 
     return $buildin;
 }
 
-sub _split_macro_queue($queue) {
+sub _split_macro_queue ($queue) {
 
     my @before_continue = ();
     while ( my $item = shift $queue->@* ) {
@@ -65,11 +66,9 @@ sub _split_macro_queue($queue) {
 sub _test_shell_cmds ( $shell, $buildin, @args ) {
 
     foreach my $queue (@args) {
+        foreach my $line ( $queue->@* ) {    # check for completeness
 
-        # check for completeness
-        foreach my $line ( $queue->@* ) {
-
-            next if exists $buildin->{$line}; # shell does not know about buildins
+            next if exists $buildin->{$line};    # shell does not know about buildins
             say "$line";
 
             # the test command below also checks for variables
@@ -85,14 +84,12 @@ sub _replace_variables ( $buildin, $variables, @queue ) {
 
     foreach my $line (@queue) {
 
-        next if exists $buildin->{$line};# shell does not know about buildins
+        next if exists $buildin->{$line};    # shell does not know about buildins
 
-        my @filtered_line = filter_input($line);
+        my @filtered_line   = filter_input($line);
         my $failed_variable = replace_variables( $variables, \@filtered_line );
 
-        if ( $failed_variable eq '1' ) {    # otherwise its a string
-            die 'ERROR: cannot execute macro, required variables not set';
-        }
+        die 'ERROR: cannot execute macro, required variables not set' if ( $failed_variable eq '1' );    # otherwise its a string
         push @substituted_queue, join( ' ', @filtered_line );
     }
 
@@ -102,13 +99,13 @@ sub _replace_variables ( $buildin, $variables, @queue ) {
 sub _save_macro_queue ( $queue_tosave, $macrosave_path ) {
 
     print 'saving queue...';
-    local ($?, $!);
+    local ( $?, $! );
     nstore $queue_tosave, $macrosave_path or die 'ERROR: could not open macro save file';
     say ' done.';
     return;
 }
 
-sub _curry_shell ( $core ) {
+sub _curry_shell ($core) {
 
     my $shell      = $core->{shell};
     my $variables  = $core->{variables};
@@ -124,15 +121,13 @@ sub _curry_shell ( $core ) {
     #
     # i also cant think of a situation where multiple SAVE/CONTINUE blocks within a macro would make sense (enter a chroot within a chroot?)
     # but it COULD make sense, so it is implemented.
-    # allthough right now, there is only ONE 'savefile' supported. so one would really need to chroot multiple times to make use of this.
+    # although right now, there is only ONE 'savefile' supported. so one would really need to chroot multiple times to make use of this.
     # might implement SAVE <name> and restore functionality once needed (which would grand a lot of headache)
     # but as it is, this whole construct is just for the sake of chroot. so... probably not :)
     return sub ($query) {
 
         my $buildin = _check_macro_buildins( $query->() );
-
-        # split queue into before and after first CONTINUE
-        my ( $queue, $toexecute ) = _split_macro_queue( $query->() );
+        my ( $queue, $toexecute ) = _split_macro_queue( $query->() );         # split queue into before and after first CONTINUE
 
         _test_shell_cmds( $shell, $buildin, $toexecute, $queue );
 
@@ -164,12 +159,10 @@ sub _curry_shell ( $core ) {
 
 sub _generate_macros ( $debug, $shell, $macro_list ) {
 
-    #   say Dumper $macro_list;
     my @macros = slice_tree( $macro_list, plugin_condition($macro_ref) );
 
     foreach my $macro (@macros) {
 
-        #    say Dumper $macro;
         my $macro_def = $macro->[0];
         my $path      = $macro->[1];
 
@@ -183,7 +176,6 @@ sub _generate_macros ( $debug, $shell, $macro_list ) {
         $macro_def->{CMD}  = $shell;
 
         say 'OK' if ($debug);
-
     }
     return $macro_list;
 }
@@ -197,4 +189,3 @@ sub load_macros ( $core, $macros ) {
     add_tree( $combined, _generate_macros( $debug, $shell, $macros ) ) if ( $macros && scalar keys $macros->%* != 0 );
     return $combined;
 }
-1;
