99d448f41ad5ab01a04f29ea29a9e5acf57ce297
Gerulf Schnitzler <github.account@goto11.at>
2024-07-12 20:11:58 +0200

    refactor Data.pm

--- Core/Plugins/Data.pm
+++ Core/Plugins/Data.pm
@@ -11,23 +11,23 @@ our @EXPORT_OK = qw(plugin_data);
 
 sub plugin_data ( $state, $data, $data_req, @arg ) {
 
-    my $cond = sub ($branch) {
-        return 1 if ref $branch->[0] eq '';
-        return 0;
-    };
-
-    my (@requests) = slice_tree( $data_req, $cond );
+    my @requests = slice_tree(
+        $data_req,
+        sub ($branch) {
+            return 1 if ref $branch->[0] eq '';
+            return 0;
+        }
+    );
 
     foreach my $request (@requests) {
 
         my $query_req     = $request->[0];                # the $query string
         my @path          = $request->[1]->@*;            # the path to that query string
-        my $is_state      = 0;
         my @split_request = split( /\s+/, $query_req );
 
         die 'ERROR: empty request' if ( scalar @split_request == 0 );
-        $is_state = 1 if ( $split_request[0] eq 'state' );
 
+        my $is_state = $split_request[0] eq 'state' ? 1 : 0;
         my $last_req = pop @path;
         my $ref      = $data_req;
 
@@ -36,30 +36,22 @@ sub plugin_data ( $state, $data, $data_req, @arg ) {
             $ref = $ref->{$key};
         }
 
-        if ($is_state) {
+        if ( !$is_state ) {
+            $ref->{$last_req} = $data->($query_req);
+            next;
+        }
 
-            # remove 'state'
-            shift @split_request;
+        shift @split_request;                    # remove 'state'
+        my $state_key = shift @split_request;    # get state hook
 
-            # get state hook
-            my $state_key = shift @split_request;
+        if ($state_key) {
 
-            if ($state_key) {
-
-                die "ERROR: requested state variable '@path' does not exist in request '$query_req'" unless ( exists( $state->{$state_key} ) );
-                $ref->{$last_req} = sub (@args) { return $state->{$state_key}->( @split_request, @args ) };
-            }
-            else {
-
-                # well, one might request ALL the state variables.
-                $ref->{$last_req} = sub { return $state };
-            }
+            die "ERROR: requested state variable '@path' does not exist in request '$query_req'" unless ( exists( $state->{$state_key} ) );
+            $ref->{$last_req} = sub (@args) { return $state->{$state_key}->( @split_request, @args ) };
         }
         else {
-            $ref->{$last_req} = $data->($query_req);
+            $ref->{$last_req} = sub { return $state };    # well, one might request ALL the state variables.
         }
     }
     return query($data_req);
 }
-
-1;
